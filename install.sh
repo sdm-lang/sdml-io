102c74343e89bcce025fb84f1707021380349d9d
Simon Johnston <johnstonskj@gmail.com>
2023-09-21 14:37:56 -0700

    fix: debugging installer

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 if [[ ! "$OSTYPE" == "darwin"* ]]; then
     echo "Unsupported operating system (today)"
     exit 1
 fi
-
-LOG=${HOME}/.install-sdml.log #/dev/null
-echo "Installer started on $(date)" >${LOG}
 
 set -- $(locale LC_MESSAGES)
 yesexpr="$1"; noexpr="$2"; yesword="$3"; noword="$4"
@@ -17,18 +14,18 @@ function install_package_manager {
     local display_name=$3
     local script_args=$4
 
-    if ! command -v ${command} 2>&1 >>${LOG}; then
-        echo "Installing ${display_name}..." | tee -a ${LOG}
+    if ! command -v ${command} 2>&1 >/dev/null; then
+        echo "Installing ${display_name}..."
         script=$(mktemp)
-        if ! curl ${curl_command} > ${script} 2>>${LOG}; then
+        if ! curl ${curl_command} > ${script}; then
             exit 2
         fi
-        if ! sh ${script} ${script_args} 2>&1 >>${LOG}; then
+        if ! sh ${script} ${script_args}; then
             exit 2
         fi
         rm ${script}
     fi
-    echo "${display_name} package manager installed" | tee -a ${LOG}
+    echo "${display_name} package manager installed"
 }
 
 function install_package {
@@ -42,14 +39,14 @@ function install_package {
         local display_name=$3
     fi
 
-    if ! command -v ${command} 2>&1 >>${LOG}; then
-        echo "Installing ${display_name}..."| tee -a ${LOG}
-        if ! ${installer} install ${package} 2>&1 >>${LOG}; then
-            echo "${installer} failed to install package ${package} for $display_name}" | tee -a ${LOG}
+    if ! command -v ${command} 2>&1 >/dev/null; then
+        echo "Installing ${display_name}..."
+        if ! ${installer} install ${package}; then
+            echo "${installer} failed to install package ${package} for ${display_name}"
             exit 3
         fi
     fi
-    echo "${display_name} command-line installed" | tee -a ${LOG}
+    echo "${display_name} command-line installed"
 }
 
 install_package_manager brew "-fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh" Homebrew
@@ -58,7 +55,7 @@ install_package brew plantuml PlantUML
 
 install_package brew dot graphviz GraphViz
 
-install_package rustup rustup-init "Rust Toolchain"
+install_package brew rustup rustup-init "Rust Toolchain"
 
 install_package cargo sdml sdml-cli SDML
 
@@ -66,31 +63,31 @@ if ! command -v emacs &>/dev/null; then
     while true; do
         read -p "Emacs is the primary SDML editor, do you wish to install (${yesword}/${noword})? " yn
         if [[ "$yn" =~ ${yesexpr} ]]; then
-            brew tap d12frosted/emacs-plus 2>&1 >>${LOG}
+            brew tap d12frosted/emacs-plus
             install_package brew emacs "emacs-plus@28" Emacs
 
             if [[ ! -d ${HOME}/.emacs.d/ ]]; then
-                if ! mkdir ${HOME}/.emacs.d 2>&1 >>${LOG}; then
+                if ! mkdir ${HOME}/.emacs.d; then
                     exit 3
                 fi
             fi
-            pushd ${HOME}/.emacs.d/ 2>&1 >>${LOG}
+            pushd ${HOME}/.emacs.d/ 2>&1 >/dev/null
 
             install_package brew git Git
 
-            git clone https://github.com/sdm-lang/tree-sitter-sdml.git 2>&1 >>${LOG} || exit 3
-            pushd tree-sitter-sdml 2>&1 >>${LOG}
+            git clone https://github.com/sdm-lang/tree-sitter-sdml.git || exit 3
+            pushd tree-sitter-sdml 2>&1 >/dev/null
             install_package brew gmake make "GNU Make"
             if [[ ! -d ${HOME}/.tree-sitter/bin/ ]]; then
-                if ! mkdir ${HOME}/.tree-sitter/bin 2>&1 >>${LOG}; then
+                if ! mkdir ${HOME}/.tree-sitter/bin; then
                     exit 3
                 fi
             fi
-            gmake emacs 2>&1 >>${LOG} || exit 3
-            popd 2>&1 >>${LOG}
+            gmake emacs  || exit 3
+            popd 2>&1 >/dev/null
 
-            git clone https://github.com/sdm-lang/emacs-sdml-mode.git sdml-mode 2>&1 >>${LOG} || exit 3
-            popd 2>&1 >>${LOG}
+            git clone https://github.com/sdm-lang/emacs-sdml-mode.git sdml-mode || exit 3
+            popd 2>&1 >/dev/null
         elif [[ "$yn" =~ ${noexpr} ]]; then
             break
         else
@@ -98,8 +95,6 @@ if ! command -v emacs &>/dev/null; then
         fi
     done
 fi
-
-echo "Finished." >>${LOG}
 
 echo ""
 sdml --help
