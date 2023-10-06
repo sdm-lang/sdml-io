c4e76448edcf0576e32437abe76df566e4e7682e
Simon Johnston <johnstonskj@gmail.com>
2023-10-06 08:44:19 -0700

    feat: better docs for tools
    Improve the implementation section with better tool descriptions.

--- README.md
+++ README.md
@@ -19,3 +19,5 @@ This repository contains the source code for the SDML website [http://sdml.io/](
 
 The documentation is all written in [Emacs](https://www.gnu.org/software/emacs/) [org-mode](https://www.gnu.org/software/emacs/) and hosted via GitHub Pages. At this time the documentation is
 generated offline and checked in so that the deployment process for Pages is a copy only.
+
+This repository also contains the install shell script for SDML tools.
--- install.sh
+++ install.sh
@@ -4,10 +4,14 @@ SUCCESS="\033[32;1m✓\033[0m"
 WARNING="\033[33;1m!\033[0m"
 ERROR="\033[31;1m✗\033[0m"
 
-if [[ ! "$OSTYPE" == "darwin"* ]]; then
-    echo "${ERROR} Unsupported operating system (today)"
-    exit 1
-fi
+case "$OSTYPE" in
+  darwin*)  INSTALL_DIR='$HOME/Library/Application Support' ;;
+  linux*)   INSTALL_DIR='$HOME/.config' ;;
+  msys*)    INSTALL_DIR='$HOMEPATH/AppData/Roaming' ;;
+  cygwin*)  INSTALL_DIR='$HOMEPATH/AppData/Roaming' ;;
+  *)        echo "${ERROR} Unsupported operating system (today)";
+            exit 1 ;;
+esac
 
 function install_package {
     local installer=$1
@@ -60,6 +64,7 @@ echo "Checking for required packages..."
 
 install_package brew plantuml PlantUML
 install_package brew dot graphviz GraphViz
+install_package brew git Git
 
 if ! command -v cargo 2>&1 >/dev/null; then
     if [[ ! -x ${HOME}/.cargo/bin/cargo ]]; then
@@ -70,71 +75,132 @@ fi
 
 install_package cargo sdml sdml-cli SDML
 
-if ! command -v femacs &>/dev/null; then
-    set -- $(locale LC_MESSAGES)
-    yesexpr="$1"; noexpr="$2"; yesword="$3"; noword="$4"
-    cat <<EOF
+cat << EOF
 
-While any text editor may be used with SDML we recommend a programmer's editor
-such as TextMate, SublimeText, or vi (vim); or an IDE such as Visual Studio Code
-or IDEA/CLion/RustRover.
 
-- https://macromates.com/
-- https://www.sublimetext.com/
-- https://www.vim.org/
-- https://code.visualstudio.com/
-- https://www.jetbrains.com/
+While any text editor may be used with SDML we recommend a programmer's
+editor such as TextMate, Sublime Text, or vi (vim); or an IDE such as
+Visual Studio Code or IDEA/CLion/RustRover. Emacs has been the primary
+SDML editor and has support for syntax highlighting and org-mode support
+for writing documentation. The SDML language documentation itself was
+written entirely in Emacs and org-mode.
 
-Emacs has been the primary SDML editor and has support for syntax highlighting
-and org-mode support for writing documentation. The SDML language documentation
-itself was written entirely in org-mode.
+Would you like to install one of the following editors and their
+respective SDML support?
+
+1 - Visual Studio Code (recommended)
+2 - Emacs and sdml-mode (most complete support)
+3 - TextMade and SDML.tmbundle
+4 - Sublime Text and SDML.tmbundle (partial support)
+5 - Neovim and tree-sitter-sdml (partial support)
+6 - IntelliJ IDEA (partial support)
+N - No editor support
 
 EOF
 
-    while true; do
-        read -p "Do you wish to install Emacs (${yesword}/${noword})? " yn
-        if [[ "$yn" =~ ${yesexpr} ]]; then
-            echo ""
+function install_emacs {
+    brew tap d12frosted/emacs-plus
+    install_package brew emacs "emacs-plus@28" Emacs
 
-            brew tap d12frosted/emacs-plus
-            install_package brew emacs "emacs-plus@28" Emacs
-
-            EMACS_HOME=$(emacsclient --eval user-emacs-directory |cut -d '"' -f 2) || exit 3
-            EMACS_HOME=${EMACS_HOME/#\~/${HOME}}
-            if [[ ! -d ${EMACS_HOME} ]]; then
-                if ! mkdir -p ${EMACS_HOME}; then
-                    exit 3
-                fi
-            fi
-            echo "${SUCCESS} Emacs user directory is ${EMACS_HOME}"
-
-            pushd ${EMACS_HOME} 2>&1 >/dev/null
-
-
-            install_package brew git Git
-
-            if [[ ! -d ${EMACS_HOME}/tree-sitter-sdml ]]; then
-                git clone https://github.com/sdm-lang/tree-sitter-sdml.git || exit 3
-                echo "${SUCCESS} Cloned tree-sitter library in ${EMACS_HOME}/tree-sitter-sdml"
-            fi
-
-            if [[ ! -d ${EMACS_HOME}/sdml-mode ]]; then
-                git clone https://github.com/sdm-lang/emacs-sdml-mode.git sdml-mode || exit 3
-                echo "${SUCCESS} Cloned sdml-mode in ${EMACS_HOME}/sdml-mode"
-            fi
-
-            echo "${SUCCESS} Checkout the documentation for instructions on Emacs configuration."
-
-            popd 2>&1 >/dev/null
-
-            break
-        elif [[ "$yn" =~ ${noexpr} ]]; then
-            break
-        else
-            echo "${WARNING} Answer ${yesword} or ${noword}."
+    EMACS_HOME=$(emacsclient --eval user-emacs-directory |cut -d '"' -f 2) || exit 3
+    EMACS_HOME=${EMACS_HOME/#\~/${HOME}}
+    if [[ ! -d ${EMACS_HOME} ]]; then
+        if ! mkdir -p ${EMACS_HOME}; then
+            exit 3
         fi
-    done
-fi
+    fi
+    echo "${SUCCESS} Emacs user directory is ${EMACS_HOME}"
+
+    pushd ${EMACS_HOME} 2>&1 >/dev/null
+
+    if [[ ! -d ${EMACS_HOME}/tree-sitter-sdml ]]; then
+        if git clone https://github.com/sdm-lang/tree-sitter-sdml.git; then
+            echo "${SUCCESS} Cloned tree-sitter library in ${EMACS_HOME}/tree-sitter-sdml"
+        else
+            echo "${ERROR} Could not clone tree-sitter library from repository";
+            exit 3
+        fi
+    fi
+
+    if [[ ! -d ${EMACS_HOME}/sdml-mode ]]; then
+        if git clone https://github.com/sdm-lang/emacs-sdml-mode.git sdml-mode; then
+            echo "${SUCCESS} Cloned sdml-mode in ${EMACS_HOME}/sdml-mode"
+        else
+            echo "${ERROR} Could not clone sdml-mode from repository";
+            exit 3
+        fi
+    fi
+
+    echo "${SUCCESS} Checkout the documentation for instructions on Emacs configuration."
+
+    popd 2>&1 >/dev/null
+}
+
+function install_textmate_bundle {
+    editor_name=$1
+    bundle_dir=$2
+
+    if [[ "$OSTYPE" == "linux"* ]]; then
+		safe_editor=$(echo "${editor}" | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
+		install_path = '${INSTALL_DIR}/${safe_editor}/${bundle_dir}'
+	else
+		install_path = '${INSTALL_DIR}/${editor}/${bundle_dir}'
+	fi
+
+	pushd ${install_path} 2>&1 >/dev/null
+	if [[ -d "./SDML.tmbundle.git" ]]; then
+		cd SDML.tmbundle
+		if git pull; then
+            echo "${SUCCESS} Updated bundle from repository"
+        else
+            echo "${ERROR} Could not update bundle from repository";
+            exit 4
+        fi
+		cd ..
+	else
+		if git clone git://github.com/sdm-lang/SDML.tmbundle.git "SDML.tmbundle"; then
+            echo "${SUCCESS} Cloned bundle from repository"
+        else
+            echo "${ERROR} Could not clone bundle from repository";
+            exit 4
+        fi
+	fi
+    popd 2>&1 >/dev/null
+}
+
+while true; do
+    read -p "Select editor to install " editor
+    case ${editor} in
+        1) install_package brew code visual-studio-code "Visual Studio Code";
+           if code --force --install-extension SimonJohnston.sdml; then
+               echo "${SUCCESS} Visual Studio Extension installed"
+           else
+               echo "${ERROR} Visual Studio Extension install failed";
+               exit 5
+           fi
+           ;;
+        2) install_emacs ;;
+        3) install_package brew mate textmate "TextMate" ;
+           install_textmate_bundle TextMate Bundless ;
+           if osascript -e 'tell app "TextMate" to reload bundles'; then
+               echo "${SUCCESS} TextMate reloading bundles"
+           else
+               echo "${ERROR} Could not tell TextMate to reload ";
+               exit 5
+           fi
+           ;;
+        4) install_package brew subl sublime-text "Sublime Text" ;
+           install_textmate_bundle "Sublime Text" Packages
+           ;;
+        5) install_package brew vim neovim "Neovim" ;
+           echo "${WARNING} Follow: some instructions"
+           ;;
+        6) install_package brew idea intellij-idea-ce "IntelliJ IDEA" ;
+           echo "${WARNING} Follow: https://www.jetbrains.com/help/idea/textmate.html"
+           ;;
+        N*) break ;;
+    esac
+done
 
 echo ""
 sdml --help
